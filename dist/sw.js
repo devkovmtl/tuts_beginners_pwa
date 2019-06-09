5d1509966bf48699bcaf7289bab4acc70d28077e
Mikael A <dev.kov.mtl@gmail.com>
2019-06-09 10:49:34 -0400

    # 15 - Pre Caching Assets

--- dist/sw.js
+++ dist/sw.js
@@ -1,7 +1,31 @@
+// CACHES NAMES
+const staticCacheName = 'site-static'
+
+// our array of assets
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/css/materialize.min.js',
+  '/css/styles.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family="Material+Icons"'
+]
+
 /// adding event listener
 // self inside service worker refere to service worker itself
 self.addEventListener('install', event => {
-  console.log('service worker has been installed')
+  // console.log('service worker has been installed')
+  // we are going to cache asset that we are not going to fetch too much and that are not going to change so much
+  // wait until the promise is resolved
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('Caching shell assets')
+      // add item to the cache
+      cache.addAll(assets)
+    })
+  )
 })
 
 // listen for the activate event from service worker!
